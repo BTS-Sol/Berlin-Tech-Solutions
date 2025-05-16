5b32a4d380221d160e521a5460b39f70cc377f88
BTS-Sol <berlinsolutions892@gmail.com>
2025-05-16 10:52:47 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
   window.addEventListener('load', () => {
     const preloader = document.getElementById('preloader');
     setTimeout(() => {
-      preloader.style.transition = 'opacity 0.5s ease';
+      preloader.style.transition = 'opacity 1.5s ease';
       preloader.style.opacity = '0';
       setTimeout(() => {
         preloader.style.display = 'none';
